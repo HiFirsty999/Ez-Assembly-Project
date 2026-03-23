865a68c5d259442d538f9baacba0898ef1051f20
Firsty <fc.firsty@gmail.com>
2026-03-23 21:58:26 +0700

    Add assembly program to print 'Hello, world!'

---
+++ main.asm
@@ -0,0 +1,23 @@
+section .data
+  txt db "Hello, world!", 0xa
+  txtlen equ $ - txt
+  
+section .text
+  global _start
+  
+_start:
+  JMP _idk
+
+  _idk:
+    mov eax, 4
+    mov ebx, 1
+  
+    mov ecx, txt
+    mov edx, txtlen
+  
+  int 0x80
+  
+  mov eax, 1
+  mov ebx, 0
+  
+  int 0x80
